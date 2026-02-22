d7aeed6afa641468493ec24484d99c5eb6dd8dab
tunasevikk-cmyk <tunasevikk@gmail.com>
2026-02-22 14:31:23 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "MAIN CALISIYOR"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
